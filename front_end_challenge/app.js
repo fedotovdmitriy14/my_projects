10dd6b358f42f1b33f5e8988318cbe7e720e7dfd
fedotovdmitriy14 <fedotovdmitriy14@gmail.com>
2021-07-15 16:02:25 +0300

    adding a front_end page

---
+++ front_end_challenge/app.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+
+const app = express()
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("The server is up and running")
+})
