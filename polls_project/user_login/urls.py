51ea996057047616d3801e8e6a5df5776de156c6
fedotovdmitriy14 <fedotovdmitriy14@gmail.com>
2021-08-18 20:16:13 +0300

    some minor changes

--- polls_project/user_login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
-from . import views
-
-urlpatterns = [
-    path('login/', obtain_auth_token, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-]
